cb0aee4cd350fa902136b4280b0580d1a235e5b8
Zsoreign <zrsanchez36@gmail.com>
2023-06-21 17:20:16 -0400

    Assets

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,6 @@
+let showSection = document.createElement('div');
+showSection.classList.add('dom__shows--container');
+
+let showsHeader = document.createElement('h1');
+showsHeader.classList.add('show__header--elem');
+showSection.appendChild(showsHeader);
