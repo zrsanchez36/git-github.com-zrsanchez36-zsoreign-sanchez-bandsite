b7a9ef220af117c0f2c1554e8e87b22e5bab02b7
Zsoreign <zrsanchez36@gmail.com>
2023-07-14 02:06:19 -0400

    fixed comment section js

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,141 +1,130 @@
-const form = document.getElementById('postComment');
+
 const submittedCommentsContainer = document.createElement('div');
 submittedCommentsContainer.setAttribute('id', 'commentHolder');
 submittedCommentsContainer.classList.add('submitted__comment--dom');
 
-let defaultComment1 = {
+let comments = [
+{
     name: 'Connor Walton',
-    comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+    text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
     timeStamp: '02/17/2021'
 
-}
+},
 
-let defaultComment2 = {
+{
     name: 'Emilie Beach',
-    comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+    text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
     timeStamp: '01/09/2021'
 
-}
+},
 
-let defaultComment3 = {
+{
     name: 'Miles Acosta',
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
     timeStamp: '12/20/2020'
 
 }
+];
 
 
-let commentArr = [defaultComment1, defaultComment2, defaultComment3];
 
 
-for (let i = 0; i < commentArr.length; i++) {
-        console.log(commentArr[i]);
-    }
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-   // console.log('formsubmitted',);
-
-    var myDate = new Date(), 
-    currentMonth = myDate.getMonth() + 1,
-    currentDay = myDate.getDate(),
-    currentYear = myDate.getFullYear();
-
-    //console.log(month,'/',day,'/',year );
-    let cmmtDate = currentMonth.toString() + '/'+ currentDay.toString() + '/' + currentYear.toString();
-    const dateTextDom = document.createElement('p');
-    dateTextDom.classList.add('date__text');
-    dateTextDom.innerText = cmmtDate;
-    console.log(cmmtDate);
+//Show already created comments on load
+window.addEventListener('load', allComments);
 
 
-    const newUserFormData = new FormData(e.target);
-    // console.log(newUserFormData);
+function displayComment(comment) {
 
-    //create data obj
-    var formDataObj = Object.fromEntries(newUserFormData.entries());
-    newUserFormData.forEach((value, key) => (formDataObj[key] = value));
-    console.log(formDataObj);
-   
-    const newUser = document.getElementById('userName').value;
-    const userComment = document.getElementById('commentBox').value;
+    //Create comment section div
+    //const commentSectionParent = document.getElementById('commentDOMSection');
+    const commentCard = document.getElementById("commentCard");
+    
 
-    const userCommentDom = document.createElement('p');
-    userCommentDom.classList.add('comment__text');
-    userCommentDom.innerText = userComment;
-
-    console.log(newUser,userComment);
-   
-   
+    const savedCommtDiv = document.createElement('div');
+    savedCommtDiv.classList.add('saved__comment--container');
+    submittedCommentsContainer.appendChild(savedCommtDiv);
 
 
     let circleContainer = document.createElement('div');
     circleContainer.classList.add('user__icon--blank'); //Blank user icon constructor
     circleContainer.setAttribute('id', 'circle');
-   
+    savedCommtDiv.appendChild(circleContainer);
+
+     //create default comment username html
+    let nameContainer = document.createElement('h3');
+    nameContainer.classList.add('comment__creator');
+    nameContainer.setAttribute('id', 'userComment');
+    nameContainer.textContent = comment.name;
+     //create default comment username html
+
+    //create default date in HTML
+    let dateContainer = document.createElement('p');
+    dateContainer.classList.add('date__text');
+    dateContainer.setAttribute('id', 'commentDate');
+    dateContainer.textContent = comment.timeStamp;
+    //create default date in HTML
 
 
 
-    const textBox = document.createElement('h3');
-    textBox.classList.add('comment__creator');
-    const commentWrapper = document.createElement('div');
-    commentWrapper.classList.add('saved__comment--container');
+    //create default comment in html
+    let commentContainer = document.createElement('p');
+    commentContainer.classList.add('comment__text');
+    commentContainer.setAttribute('id', 'commentText');
+    commentContainer.textContent = comment.text;
+    //create default comment in html
+
     
-    textBox.innerHTML = newUser;
-    const commentSectionParent = document.getElementById('commentHolder');
-    commentSectionParent.appendChild(commentWrapper);
-    commentWrapper.append(circleContainer, textBox, dateTextDom, userCommentDom);
-    
-})
-   
+    savedCommtDiv.appendChild(circleContainer);
+    savedCommtDiv.appendChild(nameContainer);
+    savedCommtDiv.appendChild(dateContainer);
+    savedCommtDiv.appendChild(commentContainer);
 
 
-function displayComment() {
-        
-        //Create comment section div
-        const commentSectionParent = document.getElementById('commentDOMSection');
-        const savedCommtDiv = document.createElement('div');
+    commentCard.appendChild(savedCommtDiv);
 
-        savedCommtDiv.classList.add('saved__comment--container');
-        commentSectionParent.appendChild(submittedCommentsContainer);
-        submittedCommentsContainer.appendChild(savedCommtDiv);
 
-        let circleContainer = document.createElement('div');
-        circleContainer.classList.add('user__icon--blank'); //Blank user icon constructor
-        circleContainer.setAttribute('id', 'circle');
-        savedCommtDiv.appendChild(circleContainer);
-
-            //create default comment username html
-        let nameContainer = document.createElement('h3');
-        nameContainer.classList.add('comment__creator');
-        nameContainer.setAttribute('id', 'userComment');
-        savedCommtDiv.appendChild(nameContainer);
-        nameContainer.innerText = defaultComment1.name;
-        //Create default comment username html
+}
 
 
 
-             //create default date in HTML
-        let dateContainer = document.createElement('p');
-        dateContainer.classList.add('date__text');
-        dateContainer.setAttribute('id', 'commentDate');
-        savedCommtDiv.appendChild(dateContainer);
-        dateContainer.innerText = defaultComment1.timeStamp;
-         //create default date in HTML
+
+function allComments() {
+    const commentCard = document.getElementById("commentCard");
+    commentCard.innerHTML = '';
+
+    comments.forEach(comment =>{
+        displayComment(comment);
+    });
+}
 
 
 
-             //create default comment in html
-        let commentContainer = document.createElement('p');
-        commentContainer.classList.add('comment__text');
-        commentContainer.setAttribute('id', 'commentText');
-        savedCommtDiv.appendChild(commentContainer);
-        commentContainer.innerText =  defaultComment1.comment;
-         //create default comment in html
-        
+function formSubmissionHandler(event) {
+    event.preventDefault();
 
+    const newUserInput = document.getElementById('userName');
+    const userComment = document.getElementById('commentBox');
+
+    const userName = newUserInput.value;
+    const comment = userComment.value;
+
+
+    if (userName && comment) {
+        const newComment = {
+            name: userName,
+            timeStamp: new Date().toLocaleDateString(),
+            text: comment
+        };
+
+        comments.push(newComment);
+
+        allComments();
+
+        newUserInput.value = '';
+        userComment.value = '';
     }
+}
 
-    displayComment(defaultComment1);
-    displayComment(defaultComment2);
-    displayComment(defaultComment3); 
+
+const form = document.getElementById('postComment');
+form.addEventListener('submit', formSubmissionHandler);
